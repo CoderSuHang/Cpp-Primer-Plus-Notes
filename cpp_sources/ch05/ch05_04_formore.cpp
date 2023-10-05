22c80e71a3ff00f8fde0aac1554778e5287cd2b4
CoderSuHang <104765251+CoderSuHang@users.noreply.github.com>
2023-10-05 22:44:43 +0800

    Add files via upload

---
+++ cpp_sources/ch05/ch05_04_formore.cpp
@@ -0,0 +1,14 @@
+/*// ch05_04_formore.cpp -- more looping with for
+#include <iostream>
+const int ARSIZE = 16;
+
+int main() {
+	long long factorials[ARSIZE];
+	factorials[1] = factorials[0] = 1LL;
+	for (int i = 2; i < ARSIZE; i++)
+		factorials[i] = i * factorials[i - 1];
+	for (int i = 0; i < ARSIZE; i++)
+		std::cout << i << "! = " << factorials[i] << std::endl;
+
+	return 0;
+}*/
