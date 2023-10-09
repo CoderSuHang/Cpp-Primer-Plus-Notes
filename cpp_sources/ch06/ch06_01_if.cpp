01c2323493e768d7535807c5451ea24a62e1624a
CoderSuHang <104765251+CoderSuHang@users.noreply.github.com>
2023-10-09 16:19:30 +0800

    Add files via upload

---
+++ cpp_sources/ch06/ch06_01_if.cpp
@@ -0,0 +1,22 @@
+/*// ch06_01_if.cpp -- using the if statement
+#include <iostream>
+int main() {
+	using std::cin;
+	using std::cout;
+
+	char ch;
+	int spaces = 0;
+	int total = 0;
+	cin.get(ch);
+	while (ch != '.') {
+		if (ch == ' ')
+			++spaces;
+		++total;
+		cin.get(ch);
+	}
+
+	cout << spaces << "spaces, " << total;
+	cout << " characters total in senternce\n";
+
+	return 0;
+}*/
---
+++ cpp_sources/ch06/ch06_02_ifelse.cpp
@@ -0,0 +1,20 @@
+/*// ch06_02_ifelse.cpp -- using the if else statement
+#include <iostream>
+int main() {
+	char ch;
+
+	std::cout << "Type, and I shall repeat.\n";
+	std::cin.get(ch);
+	
+	while (ch != '.') {
+		if (ch == '\n')
+			std::cout << ch;
+		else
+			std::cout << ++ch;
+		std::cin.get(ch);
+	}
+
+	std::cout << "\nPlease excuse the slight confusion.\n";
+
+	return 0;
+}*/
