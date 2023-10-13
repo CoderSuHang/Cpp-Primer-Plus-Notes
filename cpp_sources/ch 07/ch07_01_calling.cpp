e32b5be8fee616054d427e8c77174775a24ceb61
CoderSuHang <104765251+CoderSuHang@users.noreply.github.com>
2023-10-13 11:55:05 +0800

    Add files via upload

---
+++ cpp_sources/ch 07/ch07_01_calling.cpp
@@ -0,0 +1,17 @@
+/*// ch07_01_calling.cpp -- defining, prototyping, and calling a fuction
+#include <iostream>
+void simple();
+
+int main() {
+	using namespace std;
+	cout << "main() will call the simple() function:\n";
+	simple();
+	cout << "main() is finished with the simple() function.\n";
+
+	return 0;
+}
+
+void simple() {
+	using namespace std;
+	cout << "I'm but a simple function.\n";
+}*/
