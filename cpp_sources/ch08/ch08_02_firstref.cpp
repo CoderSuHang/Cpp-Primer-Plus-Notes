56aab71a85e80ec20bff2e7e6a2d6325f7b5fd38
CoderSuHang <104765251+CoderSuHang@users.noreply.github.com>
2023-10-19 11:42:58 +0800

    Add files via upload

---
+++ cpp_sources/ch08/ch08_02_firstref.cpp
@@ -0,0 +1,19 @@
+/*// ch08_02_firstref.cpp -- defning and using a reference
+#include <iostream>
+int main() {
+	using namespace std;
+	int rats = 101;
+	int& rodents = rats;
+
+	cout << "rats = " << rats;
+	cout << ", rodents = " << rodents << endl;
+
+	rodents++;
+	cout << "rats = " << rats;
+	cout << ", rodents = " << rodents << endl;
+
+	cout << "rats address = " << &rats;
+	cout << ", rodents address = " << &rodents << endl;
+
+	return 0;
+}*/
