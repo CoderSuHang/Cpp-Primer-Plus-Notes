3036d784ffa1de37714f7f223adf6c7580c12ca1
CoderSuHang <104765251+CoderSuHang@users.noreply.github.com>
2023-10-31 11:20:35 +0800

    Add files via upload

---
+++ cpp_sources/ch09/coordin.h
@@ -0,0 +1,21 @@
+// coordin.h -- structyre templates and function prototypes
+// structure templates
+
+#ifndef COORDIN_H_
+#define COORDIN_H_
+
+struct polar {
+	double distance;
+	double angle;
+};
+
+struct rect {
+	double x;
+	double y;
+};
+
+// prototypes
+polar rect_to_polar(rect xypos);
+void show_polar(polar dapos);
+
+#endif
