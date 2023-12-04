46a5656333d1c226e5b46aa826e7c73d1b639dcc
CoderSuHang <104765251+CoderSuHang@users.noreply.github.com>
2023-12-04 10:43:05 +0800

    Add files via upload

---
+++ cpp_sources/ch10/ch10_07_stock20.h
@@ -0,0 +1,30 @@
+#pragma once
+
+// ch10_07_stock20.h -- augmented version
+
+#ifndef CH10_O7_STOCK20_H_
+#define CH10_O7_STOCK20_H_
+
+#include <string>
+
+class Stock
+{
+private:
+	std::string company;
+	long shares;
+	double share_val;
+	double total_val;
+	void set_tot() { total_val = shares * share_val; }
+public:
+	// two constructors
+	Stock();		// default constructor
+	Stock(const std::string& co, long n = 0, double pr = 0.0);
+	~Stock();		// noisy destructor
+	void buy(long num, double price);
+	void sell(long num, double price);
+	void update(double price);
+	void show()const;
+	const Stock& topval(const Stock& s)const;
+};
+
+#endif
