07f6432d38c89720c98cbe1540a350891cd49925
CoderSuHang <104765251+CoderSuHang@users.noreply.github.com>
2023-12-06 15:54:48 +0800

    Add files via upload

---
+++ cpp_sources/ch11/ch11_13_vector.h
@@ -0,0 +1,47 @@
+#pragma once
+// ch11_13_vector.h -- Vector class with <<, mode state
+#ifndef CH11_13_VECTOR_H_ 
+#define CH11_13_VECTOR_H_
+#include <iostream>
+
+namespace VECTOR
+{
+	class Vector
+	{
+	public:
+		enum Mode { RECT, POL };
+	// RECT for rectangular, POL for Polar modes
+	private:
+		double x;
+		double y;
+		double mag;
+		double ang;
+		Mode mode;
+	// private methods for setting values
+		void set_mag();
+		void set_ang();
+		void set_x();
+		void set_y();
+	public:
+		Vector();
+		Vector(double n1, double n2, Mode form = RECT);
+		void reset(double n1, double n2, Mode form = RECT);
+		~Vector();
+		double xval() const { return x; }
+		double yval() const { return y; }
+		double magval() const { return mag; }
+		double angval() const { return ang; }
+		void polar_mode();
+		void rect_mode();
+	// opetator overloading
+		Vector operator+(const Vector& b) const;
+		Vector operator-(const Vector& b) const;
+		Vector operator-() const;
+		Vector operator*(double n) const;
+	// friends
+		friend Vector operator*(double n, const Vector& a);
+		friend std::ostream& operator<<(std::ostream& os, const Vector& v);
+	};
+}
+
+#endif
