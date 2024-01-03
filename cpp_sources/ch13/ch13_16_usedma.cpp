b3ba99d72f2d1f0be10f68751d57296d8813eb45
CoderSuHang <104765251+CoderSuHang@users.noreply.github.com>
2024-01-03 15:43:56 +0800

    Add files via upload

---
+++ cpp_sources/ch13/ch13_16_usedma.cpp
@@ -0,0 +1,29 @@
+// ch13_16_usedma.cpp -- inheritance, friends, and DMA
+// compile with ch13_15_dma.cpp
+
+#include <iostream>
+#include "ch13_14_dma.h"
+
+int main()
+{
+	using std::cout;
+	using std::endl;
+
+	baseDMA shirt("Portabelly", 8);
+	lacksDMA balloon("red", "Blimpo", 4);
+	hasDMA map("Mercator", "Buffalo Keys", 5);
+	cout << "Displaying baseDMA object:\n";
+	cout << shirt << endl;
+	cout << "Displaying lacksDMA object:\n";
+	cout << balloon << endl;
+	cout << "Displaying hasDMA object:\n";
+	cout << map << endl;
+	lacksDMA balloon2(balloon);
+	cout << "Result of lacksDMA copy:\n";
+	cout << balloon2 << endl;
+	hasDMA map2;
+	map2 = map;
+	cout << "Result of hasDMA assignment:\n";
+	cout << map2 << endl;
+	return 0;
+}
